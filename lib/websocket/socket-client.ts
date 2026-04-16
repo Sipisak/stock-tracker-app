cc5eb51b7775b0f8ecffad8598d1bee45fa5297e
Marek Šípek <123970857+Sipisak@users.noreply.github.com>
2026-04-16 16:50:08 +0200

    Add websocket transport option to socket client

--- lib/websocket/socket-client.ts
+++ lib/websocket/socket-client.ts
@@ -4,4 +4,5 @@ import { io } from "socket.io-client";
 export const socket = io(process.env.NEXT_PUBLIC_WS_URL || "http://localhost:4001", {
     path: "/socket.io",
     autoConnect: false,
+    transports: ["websocket"],
 });
